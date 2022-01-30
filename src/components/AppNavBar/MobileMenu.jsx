86af0577a2cd87ab928934a1fd4a0adb86ac41ac
kingwell47 <joeldoctor47@gmail.com>
2022-01-30 18:42:40 +0800

    added mobile menu and prelim overlay

--- src/components/AppNavBar/MobileMenu.jsx
+++ src/components/AppNavBar/MobileMenu.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const MobileMenu = () => {
   return (
-    <div className="mobile_menu hide-for-desktop">
-      <ul className="mobile_menu__links">
-        <li className="mobile_menu__link">Pricing</li>
-        <li className="mobile_menu__link">Product</li>
-        <li className="mobile_menu__link">About Us</li>
-        <li className="mobile_menu__link">Careers</li>
-        <li className="mobile_menu__link">Community</li>
-      </ul>
-    </div>
+    <>
+      <div className="overlay hide-for-desktop" />
+      <div className="mobile_menu hide-for-desktop">
+        <ul className="mobile_menu__links">
+          <li className="mobile_menu__link">Pricing</li>
+          <li className="mobile_menu__link">Product</li>
+          <li className="mobile_menu__link">About Us</li>
+          <li className="mobile_menu__link">Careers</li>
+          <li className="mobile_menu__link">Community</li>
+        </ul>
+      </div>
+    </>
   );
 };
 
