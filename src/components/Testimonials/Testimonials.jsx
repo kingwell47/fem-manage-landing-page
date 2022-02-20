0065c3668610a13e742e0240ef2231f11a2fa42c
kingwell47 <joeldoctor47@gmail.com>
2022-02-20 11:55:08 +0800

    preliminary testimonials styling

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,45 +1,44 @@
 import React from "react";
+import "./Testimonials.scss";
+import TestimonialsCard from "./TestimonialsCard";
+
+const TESTIMONIALS_DATA = [
+  {
+    avatar: "anisha.png",
+    name: "Anisha Li",
+    text: `“Manage has supercharged our team’s workflow. The ability to maintain
+    visibility on larger milestones at all times keeps everyone
+    motivated.”`,
+  },
+  {
+    avatar: "ali.png",
+    name: "Ali Bravo",
+    text: `“We have been able to cancel so many other subscriptions since using
+    Manage. There is no more cross-channel confusion and everyone is much
+    more focused.”`,
+  },
+  {
+    avatar: "richard.png",
+    name: "Richard Watts",
+    text: `“Manage allows us to provide structure and process. It keeps us organized 
+    and focused. I can’t stop recommending them to everyone I talk to!”`,
+  },
+  {
+    avatar: "shanai.png",
+    name: "Shanai Gough",
+    text: `“Their software allows us to track, manage and collaborate on our
+    projects from anywhere. It keeps the whole team in-sync without being
+    intrusive.”`,
+  },
+];
 
 const Testimonials = () => {
   return (
     <div className="testimonials">
       <div className="testimonials__heading">What they’ve said</div>
-      <div className="testimonials__card">
-        <img src="" alt="avatar" className="testimonials__card_avatar" />
-        <div className="testimonials__card_name">Anisha Li</div>
-        <div className="testimonials__card_text">
-          “Manage has supercharged our team’s workflow. The ability to maintain
-          visibility on larger milestones at all times keeps everyone
-          motivated.”
-        </div>
-      </div>
-      <div className="testimonials__card">
-        <img src="" alt="avatar" className="testimonials__card_avatar" />
-        <div className="testimonials__card_name">Ali Bravo</div>
-        <div className="testimonials__card_text">
-          “We have been able to cancel so many other subscriptions since using
-          Manage. There is no more cross-channel confusion and everyone is much
-          more focused.”
-        </div>
-      </div>
-      <div className="testimonials__card">
-        <img src="" alt="avatar" className="testimonials__card_avatar" />
-        <div className="testimonials__card_name">Richard Watts</div>
-        <div className="testimonials__card_text">
-          “Manage allows us to provide structure and process. It keeps us
-          organized and focused. I can’t stop recommending them to everyone I
-          talk to!”
-        </div>
-      </div>
-      <div className="testimonials__card">
-        <img src="" alt="avatar" className="testimonials__card_avatar" />
-        <div className="testimonials__card_name">Shanai Gough</div>
-        <div className="testimonials__card_text">
-          “Their software allows us to track, manage and collaborate on our
-          projects from anywhere. It keeps the whole team in-sync without being
-          intrusive.”
-        </div>
-      </div>
+      {TESTIMONIALS_DATA.map((item, index) => (
+        <TestimonialsCard {...item} key={index} />
+      ))}
       <button className="testimonials__cta">Get Started</button>
     </div>
   );
