da68ad93696ba8806af3d45b155952d74fe4df5e
kingwell47 <joeldoctor47@gmail.com>
2022-02-21 17:18:41 +0800

    footer arrangements

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,25 +15,6 @@ const Footer = () => {
     <footer className="footer">
       <FooterCTA />
       <div className="footer__bottom">
-        <img src="" alt="Manage" className="footer__logo" />
-        <ul className="footer__socials">
-          <li className="footer__social">FB</li>
-          <li className="footer__social">YT</li>
-          <li className="footer__social">TW</li>
-          <li className="footer__social">PN</li>
-          <li className="footer__social">IG</li>
-        </ul>
-        <ul className="footer__links">
-          <li className="footer__link">Home</li>
-          <li className="footer__link">Pricing</li>
-          <li className="footer__link">Products</li>
-          <li className="footer__link">About Us</li>
-        </ul>
-        <ul className="footer__links">
-          <li className="footer__link">Careers</li>
-          <li className="footer__link">Community</li>
-          <li className="footer__link">Privacy Policy</li>
-        </ul>
         <form className="footer__subscribe">
           <input
             type="text"
@@ -42,9 +23,28 @@ const Footer = () => {
           />
           <button className="footer__subscribe_button">Go</button>
         </form>
-        <div className="footer__copyright">
-          Copyright 2020. All Rights Reserved
+        <div className="footer__links_container">
+          <ul className="footer__links">
+            <li className="footer__link">Home</li>
+            <li className="footer__link">Pricing</li>
+            <li className="footer__link">Products</li>
+            <li className="footer__link">About Us</li>
+          </ul>
+          <ul className="footer__links">
+            <li className="footer__link">Careers</li>
+            <li className="footer__link">Community</li>
+            <li className="footer__link">Privacy Policy</li>
+          </ul>
         </div>
+        <ul className="footer__socials">
+          <li className="footer__social">FB</li>
+          <li className="footer__social">YT</li>
+          <li className="footer__social">TW</li>
+          <li className="footer__social">PN</li>
+          <li className="footer__social">IG</li>
+        </ul>
+        <img src="" alt="Manage" className="footer__logo" />
+        <p className="footer__copyright">Copyright 2020. All Rights Reserved</p>
       </div>
     </footer>
   );
